8203e0eb5020c3340bf7e45481c30c84731b8821
bitcoinuser <bitcoinuser@users.noreply.github.com>
2025-06-22 22:56:26 -0300

    feat: update pt-BR.ts (#402)

--- src/i18n/locales/pt-BR.ts
+++ src/i18n/locales/pt-BR.ts
@@ -37,7 +37,7 @@ export default {
     Cancel: 'Cancelar',
     Mentions: 'Menções',
     'Failed to post': 'Falha ao postar',
-    'Post successful': 'Publicada com sucesso',
+    'Post successful': 'Nota publicada com sucesso',
     'Your post has been published': 'Sua nota foi publicada',
     Repost: 'Repostar',
     Quote: 'Citar',
